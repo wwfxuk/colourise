3339b072f1588508bc95ba3a8b2229e38b253693
Joseph Yu <joseph.yu@wwfx.co.uk>
2019-10-25 18:16:13 +0100

    Back to linux and osx only

--- package.py
+++ package.py
@@ -11,8 +11,8 @@ description = (
 
 authors = ['WWFX UK']
 
-# # Technically needs bash. Tested rez on Windows, Linux and OSX CI with bash
-# requires = []
+# Technically needs bash. Tested rez on Linux and OSX CI with bash
+requires = ['platform-linux|osx']
 
 tools = ['colourise', 'colour-test']  # Names of executables from this package
 # ---- OR ----
